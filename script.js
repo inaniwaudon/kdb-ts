322b532b76989c0f94f0623d49ac0544ac020dd3
inaniwaudon <w.yuto0916@gmail.com>
2021-06-03 13:11:49 +0900

    implement the timetable

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+let subjectMap = {};
+
 window.onload = function () {
 	const table = document.querySelector("table#body tbody");
 	const keyword_input = document.querySelector("input[type=\"text\"]");
@@ -88,17 +90,6 @@ window.onload = function () {
 		checkAsNeeded.checked = false;
 	});
 
-
-	// timetable
-	const createTimeTable = (filled) => {
-		let table = new Array(daysofweek.length);
-		for (let i in daysofweek)
-			table[i] = new Array(maxPeriod).fill(filled);
-		return table;
-	};
-
-	const daysofweek = ["月", "火", "水", "木", "金", "土", "日"];
-	const maxPeriod = 6;
 	let timetablePeriods = createTimeTable(null);
 	let selectedPeriods = createTimeTable(false);
 
@@ -233,22 +224,22 @@ window.onload = function () {
 		let tr = document.createElement("tr");
 		table.appendChild(tr);
 
-		let url = `https://kdb.tsukuba.ac.jp/syllabi/2021/${line[0]}/jpn`;
-		let methods = ["対面", "オンデマンド", "同時双方向"].filter(it => line[10].indexOf(it) > -1);
+		let url = `https://kdb.tsukuba.ac.jp/syllabi/2021/${line.code}/jpn`;
+		let methods = ["対面", "オンデマンド", "同時双方向"].filter(it => line.note.indexOf(it) > -1);
 
-		tr.innerHTML += `<td>${line[0]}<br/>${line[1]}<br/><a href="${url}" class="syllabus" target="_blank">シラバス</a><input type="checkbox" onclick='onBookmarkChanged(event);' class="bookmark" id="bookmark-${line[0]}" value="${line[0]}" /></td>`;
-		tr.innerHTML += `<td>${line[3]}単位<br/>${line[4]}年次</td>`;
-		tr.innerHTML += `<td>${line[5]}<br/>${line[6]}</td>`;
-		tr.innerHTML += `<td>${line[7].replace(/,/g, "<br/>")}</td>`;
-		tr.innerHTML += `<td>${line[8].replace(/,/g, "<br/>")}</td>`;
+		tr.innerHTML += `<td>${line.code}<br/>${line.name}<br/><a href="${url}" class="syllabus" target="_blank">シラバス</a><input type="checkbox" onclick='onBookmarkChanged(event);' class="bookmark" id="bookmark-${line.code}" value="${line.code}" /></td></td>`;
+		tr.innerHTML += `<td>${line.credit}単位<br/>${line.year}年次</td>`;
+		tr.innerHTML += `<td>${line.module}<br/>${line.periodStr}</td>`;
+		tr.innerHTML += `<td>${line.room.replace(/,/g, "<br/>")}</td>`;
+		tr.innerHTML += `<td>${line.person.replace(/,/g, "<br/>")}</td>`;
 
 		if (methods.length < 1)
 			tr.innerHTML += "<td>不詳</td>"
 		else
 			tr.innerHTML += `<td>${methods.join('<br/>')}<br /></td>`;
 
-		tr.innerHTML += `<td>${line[9]}</td>`;
-		tr.innerHTML += `<td>${line[10]}</td>`;
+		tr.innerHTML += `<td>${line.abstract}</td>`;
+		tr.innerHTML += `<td>${line.note}</td>`;
 	}
 
 
@@ -271,55 +262,31 @@ window.onload = function () {
 			}
 
 			// keyword
-			let matchesNo = checkNo.checked ? line[0].indexOf(options.keyword) != 0 : true;
-			let matchesName = checkName.checked ? line[1].match(regex) == null : true;
-			let matchesRoom = checkRoom.checked ? line[7].match(regex) == null : true;
-			let matchesPerson = checkPerson.checked ? line[8].match(regex) == null : true;
-			let matchesAbstract = checkAbstract.checked ? line[9].match(regex) == null : true;
+			let matchesNo = checkNo.checked ? line.code.indexOf(options.keyword) != 0 : true;
+			let matchesName = checkName.checked ? line.name.match(regex) == null : true;
+			let matchesRoom = checkRoom.checked ? line.room.match(regex) == null : true;
+			let matchesPerson = checkPerson.checked ? line.person.match(regex) == null : true;
+			let matchesAbstract = checkAbstract.checked ? line.abstract.match(regex) == null : true;
 
 			let matchesKeyword = options.keyword != "" &&
 				(matchesNo && matchesName && matchesRoom && matchesPerson && matchesAbstract);
 
 			// period
-			if (line.length == 14) {
-				let periods = {
-					concentration : line[6].indexOf("集中") > -1,
-					negotiable : line[6].indexOf("応談") > -1,
-					asneeded : line[6].indexOf("随時") > -1,
-					period: createTimeTable(false)
-				};
-				let originalPeriods = line[6].split(/[, ]/);
-				let day = null;
-				for (let period of originalPeriods) {
-					let firstChar = period.substr(0,1);
-					if (daysofweek.includes(firstChar))
-						day = daysofweek.indexOf(firstChar);
-
-					let time_str = period.replace(/[^0-9]/g, "");
-					if (time_str.length > 0) {
-						let time = Number(time_str);
-						if (day != null)
-							periods.period[day][time-1] = true;
-					}
-				}
-				line.push(periods);
-			}
-
 			let missMatchesPeriod = true;
 			let isNotSpecifiedPeriod = true;
 			for (let day in options.period) {
 				for (let time in options.period[day]) {
 					if (options.period[day][time]) {
 						isNotSpecifiedPeriod = false;
-						if (line[14].period[day][time])
+						if (line.period.period[day][time])
 							missMatchesPeriod = false;
 					}
 				}
 			}
 
-			if ((options.concentration && line[14].concentration) ||
-				(options.negotiable && line[14].negotiable) ||
-				(options.asneeded && line[14].asneeded))
+			if ((options.concentration && line.period.concentration) ||
+				(options.negotiable && line.period.negotiable) ||
+				(options.asneeded && line.period.asneeded))
 				missMatchesPeriod = false;
 
 
@@ -327,23 +294,23 @@ window.onload = function () {
 				missMatchesPeriod = false;
 
 			// other options
-			let missMatchesSeason = options.season != "null" && line[5].indexOf(options.season) < 0;
-			let missMatchesModule = options.module_ != "null" && line[5].indexOf(options.module_) < 0;
-			let missMatchesOnline = options.online != "null" && line[10].indexOf(options.online) < 0;
-			let missMatchesBookmark = options.bookmark && !bookmarks.includes(line[0])
+			let missMatchesSeason = options.season != "null" && line.module.indexOf(options.season) < 0;
+			let missMatchesModule = options.module_ != "null" && line.module.indexOf(options.module_) < 0;
+			let missMatchesOnline = options.online != "null" && line.note.indexOf(options.online) < 0;
+			let missMatchesBookmark = options.bookmark && !bookmarks.includes(line.code)
 
 			let missMatchesYear;
-			if (line[4].indexOf("-") < 0) {
-				missMatchesYear = options.year != "null" && line[4].indexOf(options.year) < 0;
-			} else {
-				let minYear = line[4].replace(/\s-\s[1-6]/g, "");
-				let maxYear = line[4].replace(/[1-6]\s-\s/g, "");
+			if (line.year.indexOf("-") < 0)
+				missMatchesYear = options.year != "null" && line.year.indexOf(options.year) < 0;
+			else {
+				let minYear = line.year.replace(/\s-\s[1-6]/g, "");
+				let maxYear = line.year.replace(/[1-6]\s-\s/g, "");
 				missMatchesYear = options.year != "null" && (options.year < minYear || maxYear < options.year);
 			}
 
-			let missMatchesReq_A = options.reqA != "null" && options.reqA != line[11];
-			let missMatchesReq_B = options.reqB != "null" && options.reqB != line[12];
-			let missMatchesReq_C = options.reqC != "null" && options.reqC != line[13];
+			let missMatchesReq_A = options.reqA != "null" && options.reqA != line.reqA;
+			let missMatchesReq_B = options.reqB != "null" && options.reqB != line.reqB;
+			let missMatchesReq_C = options.reqC != "null" && options.reqC != line.reqC;
 			
 			if (
 				matchesKeyword ||
@@ -361,7 +328,7 @@ window.onload = function () {
 			createLine(line);
 
 			// Make bookmarked buttons active
-			document.getElementById("bookmark-" + line[0]).checked = getBookmarks().includes(line[0])
+			document.getElementById("bookmark-" + line.code).checked = getBookmarks().includes(line.code)
 
 			timeout = setTimeout(() => updateTable(options, index + 1, ++displayedIndex), 0);
 			break;
@@ -513,6 +480,89 @@ window.onload = function () {
 		data = kdbJson.subject;
 		updated = kdbJson.updated;
 
+		// convert into a map
+		for (let i in data) {
+			let line = data[i];
+			line = {
+				code: line[0],
+				name: line[1],
+				credit: line[3],
+				year: line[4],
+				module: line[5],
+				periodStr: line[6],
+				room : line[7],
+				person: line[8],
+				abstract: line[9],
+				note: line[10],
+				reqA: line[11],
+				reqB: line[12],
+				reqC: line[13]
+			};
+
+			// term (season - module)
+			line.terms = {
+				holiday: { spring: false, summer: false, autumn: false, winter: false },
+				normal: {
+					spring: { a: false, b: false, c: false },
+					autumn: { a: false, b: false, c: false }
+				},
+				normalNo: []
+			};
+
+			let seasonMap = {
+				"春": "spring",
+				"夏": "summer",
+				"秋": "autumn",
+				"冬": "winter"
+			};
+			// holiday
+			if (line.module.indexOf("休業") > -1) {
+				for (let season in seasonMap)
+					if (line.module.indexOf(season) > -1)
+						line.terms.holiday[seasonMap[season]] = true;
+			}
+			// normal
+			else {
+				let termArray = Array.from(line.module);
+				for (let char of termArray) {
+					if (char == "春" || char == "秋")
+						season = char;
+					if ((char == "A" || char == "B" || char == "C") && season) {
+						line.terms.normal[seasonMap[season]][char.toLowerCase()] = true;
+						let no = season == "春" ? 0 : 3;
+						no += char == "A" ? 0 : (char == "B" ? 1 : 2);
+						line.terms.normalNo.push(no);
+					}
+				}
+			}
+
+			// period
+			line.period = {
+				concentration : line.periodStr.indexOf("集中") > -1,
+				negotiable : line.periodStr.indexOf("応談") > -1,
+				asneeded : line.periodStr.indexOf("随時") > -1,
+				period: createTimeTable(false)
+			};
+
+			let originalPeriods = line.periodStr.split(/[, ]/);
+			let day = null;
+			for (let period of originalPeriods) {
+				let firstChar = period.charAt(0);
+				if (daysofweek.includes(firstChar))
+					day = daysofweek.indexOf(firstChar);
+
+				let time_str = period.replace(/[^0-9]/g, "");
+				if (time_str.length > 0) {
+					let time = Number(time_str);
+					if (day != null)
+						line.period.period[day][time-1] = true;
+				}
+			}
+
+			data[i] = line;
+			subjectMap[line.code] = line;
+		}
+
 		search(null);
 		updatedDate.innerHTML = updated;
 	})();
@@ -541,7 +591,22 @@ window.onload = function () {
 		timetableLink.addEventListener(supportsTouch ? "touchstart" : "click", timetableListener);
 		clearButton.addEventListener('click', clearButtonListener);
 	},{ passive: true });
+
+	new bookmarkTimetable();
 };
+
+
+// timetable
+const daysofweek = ["月", "火", "水", "木", "金", "土", "日"];
+const maxPeriod = 6;
+
+const createTimeTable = (filled) => {
+	let table = new Array(daysofweek.length);
+	for (let i in daysofweek)
+		table[i] = new Array(maxPeriod).fill(filled);
+	return table;
+};
+
 
 // Bookmark
 function getBookmarks() {
@@ -595,4 +660,146 @@ function onBookmarkChanged(event) {
 
 	if (event.target.checked) addBookmark(subjectId);
 	else removeBookmark(subjectId);
+}
+
+function removeAllBookmarks() {
+	/*let trs = document.querySelectorAll(`table#body a.bookmarked`);
+	for (let tr of trs)
+		tr.classList.remove("bookmarked");
+
+	this.subjects = [];
+	this.update();*/
+}
+
+
+// timetable displaying blookmarked subjects
+class bookmarkTimetable {
+	constructor() {
+		this.module = 0;
+		this.maxModule = 6;
+		this.periodItems = [];
+		this.displaysTimetable = false;
+
+		this.main = document.querySelector("#bookmark-timetable .main");
+		this.timetable = document.querySelector("#bookmark-timetable ul.table-list");
+		this.moduleDisplay = document.querySelector("#current-status .module");
+		this.credit = document.querySelector("#current-status .credit");
+
+		// button
+		const clearBookmark = document.querySelector("#clear-bookmarks");
+		clearBookmark.addEventListener("click", () => this.removeAll());
+
+		const previous = document.querySelector("#current-status .previous");
+		const next = document.querySelector("#current-status .next");
+		const close = document.querySelector("#close-bookmark-table");
+		previous.addEventListener("click", () => this.shiftTimetable(false));
+		next.addEventListener("click", () => this.shiftTimetable(true));
+		close.addEventListener("click", () => this.switchDisplayTimetable());
+
+		// create tables
+		for (let termNo = 0; termNo < this.maxModule; termNo++) {
+			let table = document.createElement("li");
+			table.className = "table tile";
+			this.periodItems[termNo] = createTimeTable();
+
+			for (let x = 0; x < daysofweek.length; x++) {
+				let row = document.createElement("ul");
+				row.className = "row";
+				
+				for (let y = -1; y < maxPeriod; y++) {
+					let item = document.createElement("li");
+					if (y == -1)
+						item.innerHTML = daysofweek[x];
+					row.appendChild(item);
+					this.periodItems[termNo][x][y] = item;
+				}
+				table.appendChild(row);
+			}
+			this.timetable.append(table);
+		}
+
+		let firstTimetable = document.querySelector("#bookmark-timetable ul.table-list li");
+		this.width = firstTimetable.clientWidth;
+
+		this.update();
+	}
+
+	switchDisplayTimetable() {
+		this.main.style.marginBottom = !this.displaysTimetable ? `calc(${-this.main.clientHeight}px + 1.8rem)` : 0;
+		this.displaysTimetable = !this.displaysTimetable;
+	}
+
+	shiftTimetable(isForward) {
+		const maxModule = 5;
+		if (isForward && this.module < maxModule)
+			this.switchTimetable(this.module + 1);
+		if (!isForward && this.module > 0)
+			this.switchTimetable(this.module - 1);
+	}
+
+	switchTimetable(moduleNo) {
+		if (this.module != moduleNo) {
+			this.module = moduleNo;
+			this.timetable.style.marginLeft = this.width * this.module * -1 + "px";
+			this.update();
+		}
+	}
+
+	update() {
+		let credit = 0.0;
+
+		// timetable
+		for (let termNo = 0; termNo < this.maxModule; termNo++) {
+			for (let y = 0; y < maxPeriod; y++) {
+				for (let x = 0; x < daysofweek.length; x++) {
+					let item = this.periodItems[termNo][x][y];
+					item.innerHTML = "";
+					let no = 0;
+
+					for (let code of this.subjects) {
+						let subject = subjectMap[code];
+						let period = subject.period.period;
+
+						if (subject.terms.normalNo.includes(termNo) && period[x][y]) {
+							let div = document.createElement("div");
+							let h = 200 + no * 20;
+							div.className = "class";
+							div.innerHTML = subject.name;
+							div.style.margin = 0.1 * (no + 1) + "rem";
+							div.style.background = `hsl(${h}, 100%, 90%, 0.8)`;
+
+							let remove = document.createElement("a");
+							remove.classList.add("remove");
+							remove.innerHTML = "×";
+							div.appendChild(remove);
+
+							div.addEventListener("mouseover", () => {
+								remove.classList.add("displayed");
+							});
+							div.addEventListener("mouseout", () => {
+								remove.classList.remove("displayed");
+							});
+							remove.addEventListener("click", () => {
+								this.remove(code);
+							})
+
+							item.appendChild(div);
+							no++;
+						}
+					}
+				}
+			}
+		}
+
+		// credit
+		for (let code of this.subjects)
+			if (!isNaN(subjectMap[code].credit))
+				credit += Number(subjectMap[code].credit);
+
+		// status
+		let season = this.module < 3 ? "春" : "秋";
+		let module_ = this.module % 3 == 0 ? "A" : (this.module % 3 == 1 ? "B" : "C");
+		this.moduleDisplay.innerHTML = season + module_;
+		this.credit.innerHTML = credit.toFixed(1) + "単位";
+	}
 }
