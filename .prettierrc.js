3262f7461bcffd8cf8c49e40783a7157da06cb82
eggplants <w10776e8w@yahoo.co.jp>
2021-06-17 08:30:53 +0900

    Add: prettierrc.js

---
+++ .prettierrc.js
@@ -0,0 +1,46 @@
+/**
+ * Prettier Configuration
+ */
+(module.exports = {
+  // デフォルト設定
+  // printWidth: 80
+  // tabWidth: 2,
+  // useTabs: false,
+  // semi: ture,
+  // singleQuote: false,
+  // quoteProps: "as-needed",
+  // jsxSingleQuote: false,
+  // trailingComma: "es5",
+  // bracketSpacing: true,
+  // arrowParens: "always",
+  // rangeStart: 0,
+  // rangeEnd: Infinity,
+  // jsxBracketSameLine: false,
+  // requirePragma: false,
+  // insertPragma: false,
+  // proseWrap: "preserve",
+  // htmlWhitespaceSensitivity: "css",
+  // vueIndentScriptAndStyle: false,
+  // endOfLine: "lf",
+  // embeddedLanguageFormatting: "auto"
+  // printWidth: 120,
+  // tabWidth: 2,
+  "useTabs": true,
+  "semi": true,
+  "singleQuote": false,
+  "quoteProps": "as-needed",
+  "jsxSingleQuote": false,
+  "trailingComma": "es5",
+  "bracketSpacing": true,
+  "arrowParens": "always",
+  "rangeStart": 0,
+  "rangeEnd": Infinity,
+  "jsxBracketSameLine": false,
+  "requirePragma": false,
+  "insertPragma": false,
+  "proseWrap": "preserve",
+  "htmlWhitespaceSensitivity": "css",
+  "vueIndentScriptAndStyle": false,
+  // endOfLine: "lf",
+  "embeddedLanguageFormatting": "auto"
+})
