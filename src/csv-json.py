fda832fdfcc24090b632395c6d58e08647c48e2a
eggplants <w10776e8w@yahoo.co.jp>
2021-06-17 08:22:49 +0900

    Add: docstring, type annotstion, and class+main to **.py

--- src/csv-json.py
+++ src/csv-json.py
@@ -1,126 +1,193 @@
+import argparse
 import csv
 import datetime
 import json
-import argparse
+from typing import Any, Dict, List, Tuple
 
-parser = argparse.ArgumentParser();
-parser.add_argument("csv", help="an input csv file")
-parser.add_argument("types", help="a text file of the relation between requirement classification and subject codes")
-parser.add_argument("contains_graduate", help="contains subjects for graduate school; \"True\" or \"False\"")
-
-args = parser.parse_args()
-input_type_list = "list.txt"
+# TODO: `Dict`型を暇があれば詳しく記述
 
 
-# subject code
-def get_subjectcode(str) :
-	code_str = str.replace("]", "").split("[")
+class KdbCSVtoJSON():
+    def __init__(self, csvpath: str, typespath: str,
+                 contains_graduate: bool = True) -> None:
+        self.csvpath = csvpath
+        self.typespath = typespath
+        self.contains_graduate = contains_graduate
+        self.types = self.__get_types()
 
-	codes = code_str[0].split("/")
-	if len(code_str) == 2 :
-		except_codes = code_str[1].split("/")
-	else :
-		except_codes = []
+        self.empty_typed_subjects: List[List[str]] = []
 
-	return codes, except_codes
+        now = datetime.datetime.now()
+        self.output = {
+            "updated": now.strftime("%Y/%m/%d"),
+            "subject": self.__get_subjects()
+        }
+
+    def get_types(self) -> Dict[str, Any]:
+        return self.types
+
+    def get_output(self) -> Dict[str, Any]:
+        return self.output
+
+    def print_empty_typed_subjects(self) -> None:
+        for s in self.empty_typed_subjects:
+            print(s)
+
+    def __get_subjectcode(self, s: str) -> Tuple[List[str], List[str]]:
+        """Get subject code
+
+        Args:
+            s (str): subject name
+
+        Returns:
+            Tuple[List[str], List[str]]: codes and except codes which are defined in given config text
+        """
+        code = s.replace("]", "").split("[")
+        if len(code) == 2:
+            except_codes = code[1].split("/")
+        else:
+            except_codes = []
+
+        codes = code[0].split("/")
+        return (codes, except_codes)
+
+    def __search_type(self, code: str, target_types: Dict[str, Any],
+                      types: List[str] = []) -> List[str]:
+        """Search the type
+
+        Args:
+            code (str): target code
+            target_types (Dict): dictionary contains info of target types
+            types (List[str]): found types
+
+        Returns:
+            List[str]: found types
+        """
+        for key in target_types:
+            target_codes = target_types[key]["codes"]
+            target_excepts = target_types[key]["except-codes"]
+
+            for target_code in target_codes:
+                is_grad = any([code.find(target_except) == 0
+                               for target_except in target_excepts])
+                if code.find(target_code) == 0 and not is_grad:
+                    types.append(key)
+                    if len(types) <= 2:
+                        target_childs = target_types[key]["childs"]
+                        self.__search_type(code, target_childs, types)
+                    else:
+                        return types
+
+        return types
+
+    def __get_types(self) -> Dict[str, Any]:
+        """Get types
+
+        Args:
+            args (argparse.Namespace): parsed result of command line args
+
+        Returns:
+            Dict: a dictionary of types
+        """
+        types = {}
+        first, second = "", ""
+        type_lines = open(self.typespath, encoding="utf-8").readlines()
+        rows = [
+            row for row in [
+                line.replace(" ,", ",").replace(", ", ",").split(",")
+                for line in type_lines
+            ] if len(row) >= 2]
+
+        for row in rows:
+            codes_str = row[0]
+            codes, except_codes = self.__get_subjectcode(
+                codes_str.replace("\t", ""))
+            name = row[1].replace("\n", "").replace("\t", "")
+            tab = codes_str.count("\t")
+
+            if tab == 0:
+                first = name
+                types[first] = {"codes": codes,
+                                "except-codes": except_codes, "childs": {}}
+            elif tab == 1:
+                second = name
+                types[first]["childs"][second] = {
+                    "codes": codes, "except-codes": except_codes, "childs": {}}
+            elif tab == 2:
+                types[first]["childs"][second]["childs"][name] = {
+                    "codes": codes, "except-codes": except_codes}
+
+        return types
+
+    def __get_subjects(self) -> List[List[str]]:
+        """Get subjects
+
+        Args:
+            csvpath (str): a csv file path
+
+        Returns:
+            List: a list of subjects
+        """
+        subjects = []
+        lines = [line for line in csv.reader(open(self.csvpath))]
+
+        for line in lines:
+            for i in range(6):
+                line.pop(11)
+
+            code = line[0]
+
+            # skip:
+            #   the header and empty lines
+            #   subjects for graduate school
+            is_grad = (len(code) > 0 and code[0] == '0'
+                       and not self.contains_graduate)
+            if code in ["科目番号", ""] or is_grad:
+                continue
+
+            # types
+            searched_types = self.__search_type(code, self.types, [])
+
+            if len(searched_types) == 0:
+                self.empty_typed_subjects.append(line)
+
+            first = searched_types[0] if len(searched_types) >= 1 else ""
+            second = searched_types[1] if len(searched_types) >= 2 else ""
+            third = searched_types[2] if len(searched_types) >= 3 else ""
+
+            line += [first, second, third]
+            subjects.append(line)
+
+        return subjects
 
 
-# search the type
-def search_type(code, target_types, types) :
-	for key in target_types :
-		target_codes = target_types[key]["codes"]
-		target_excepts = target_types[key]["except-codes"]
+def parse_args() -> argparse.Namespace:
+    parser = argparse.ArgumentParser()
+    parser.add_argument("csv", help="an input csv file")
+    parser.add_argument(
+        "types", help="a text file of the relation between requirement classification and subject codes")
+    parser.add_argument("-c", "--contains_graduate", action="store_true",
+                        help="contains subjects for graduate school")
 
-		for target_code in target_codes :
-			if code.find(target_code) == 0 :
-				is_except = False
-				
-				for target_except in target_excepts :
-					if code.find(target_except) == 0:
-						is_except = True
-
-				if not is_except :
-					types.append(key)
-					if len(types) <= 2 :
-						target_childs = target_types[key]["childs"]
-						search_type(code, target_childs, types)
-					else :
-						return types
-
-	return types
+    return parser.parse_args()
 
 
-# type list
-types = {}
-first = second = None
+def main() -> None:
+    """main
+    """
+    args = parse_args()
+    csvpath, typespath, contains_graduate = args.csv, args.types, args.contains_graduate
+    k = KdbCSVtoJSON(csvpath, typespath, contains_graduate)
 
-with open(args.types, encoding="utf-8") as fp:
-	type_lines = fp.readlines()
+    # output
+    with open("../kdb.json", "w", encoding="utf-8") as fp:
+        json.dump(k.get_output(), fp, indent="\t", ensure_ascii=False)
 
-	for line in type_lines :
-		rows = line.replace(" ,", ",").replace(", ", ",").split(",")
+    with open("../code-types.json", "w", encoding="utf-8") as fp:
+        json.dump(k.get_types(), fp, indent="\t", ensure_ascii=False)
 
-		# skip empty lines
-		if len(rows) < 2 :
-			continue
-
-		codes_str = rows[0]
-		codes, except_codes = get_subjectcode(codes_str.replace("\t", ""))
-		name = rows[1].replace("\n", "").replace("\t", "")
-		tab = codes_str.count("\t")
-
-		if tab == 0 :
-			first = name
-			types[first] = {"codes" : codes, "except-codes" : except_codes, "childs" : {}}
-		if tab == 1 :
-			second = name
-			types[first]["childs"][second] = {"codes" : codes, "except-codes" : except_codes, "childs" : {}}
-
-		if tab == 2 :
-			types[first]["childs"][second]["childs"][name] = {"codes" : codes, "except-codes" : except_codes}
+    k.print_empty_typed_subjects()
 
 
-# csv
-date = datetime.datetime.now()
-output = {
-	"updated": "%d/%02d/%02d"% (date.year, date.month, date.day),
-	"subject": []
-}
-
-with open(args.csv) as fp :
-	reader = csv.reader(fp)
-
-	for line in reader :
-		for i in range(6) :
-			line.pop(11)
-
-		code = line[0]
-
-		# skip the header and empty lines
-		if code in ["科目番号", ""] :
-			continue
-
-		# subjects for graduate school
-		if code[0] == '0' and args.contains_graduate == "False" :
-			continue
-
-		# types
-		searched_types = search_type(code, types, [])
-
-		if len(searched_types) == 0 :
-			print(line)
-
-		first = searched_types[0] if len(searched_types) >= 1 else ""
-		second = searched_types[1] if len(searched_types) >= 2 else ""
-		third = searched_types[2] if len(searched_types) >= 3 else ""
-
-		line += [first, second, third]
-		output["subject"].append(line)
-
-
-# output
-with open("../kdb.json", "w", encoding="utf-8") as fp :
-	json.dump(output, fp, indent="\t", ensure_ascii=False)
-
-with open("../code-types.json", "w", encoding="utf-8") as fp :
-	json.dump(types, fp, indent="\t", ensure_ascii=False)
+if __name__ == '__main__':
+    main()
--- src/download.py
+++ src/download.py
@@ -1,59 +1,127 @@
-import os
-import datetime
+from typing import Optional, TypedDict
+
 import requests
-import urllib.parse
 
-year = 2021
-post = {
-	"index": "",
-	"locale": "",
-	"nendo": year,
-	"termCode": "",
-	"dayCode": "",
-	"periodCode": "",
-	"campusCode": "",
-	"hierarchy1": "",
-	"hierarchy2": "",
-	"hierarchy3": "",
-	"hierarchy4": "",
-	"hierarchy5": "",
-	"freeWord": "",
-	"_orFlg": 1,
-	"_andFlg": 1,
-	"_gaiyoFlg": 1,
-	"_risyuFlg": 1,
-	"_excludeFukaikoFlg": 1,
-}
 
-requests.packages.urllib3.util.ssl_.DEFAULT_CIPHERS += "HIGH:!DH:!aNULL"
+class PostDict(TypedDict):
+    """Dict for requesting with POST to KdB
+    """
+    index: str
+    locale: str
+    nendo: int
+    termCode: str
+    dayCode: str
+    periodCode: str
+    campusCode: str
+    hierarchy1: str
+    hierarchy2: str
+    hierarchy3: str
+    hierarchy4: str
+    hierarchy5: str
+    freeWord: str
+    _orFlg: int
+    _andFlg: int
+    _gaiyoFlg: int
+    _risyuFlg: int
+    _excludeFukaikoFlg: int
+    _eventId: Optional[str]
+    outputFormat: Optional[int]
 
-kdb_url = "https://kdb.tsukuba.ac.jp/"
-session = requests.session()
-response = session.get(kdb_url)
 
-do_url = response.url
-qs = urllib.parse.urlparse(do_url).query
-query_dict = urllib.parse.parse_qs(qs)
+class KdbDownloader():
+    def __init__(self, year: int = 2021) -> None:
+        """Initializer
 
-# search
-search_post = post.copy()
-search_post["_eventId"] = "searchOpeningCourse"
-response = session.post(do_url, data=search_post)
-do_url = response.url
+        Args:
+            year (int, optional): a year of syllabus. Defaults to 2021.
+        """
+        requests.packages.urllib3.util.ssl_.DEFAULT_CIPHERS += "HIGH:!DH"
+        self.year = year
+        self.post: PostDict = {
+            "index": "",
+            "locale": "",
+            "nendo": year,
+            "termCode": "",
+            "dayCode": "",
+            "periodCode": "",
+            "campusCode": "",
+            "hierarchy1": "",
+            "hierarchy2": "",
+            "hierarchy3": "",
+            "hierarchy4": "",
+            "hierarchy5": "",
+            "freeWord": "",
+            "_orFlg": 1,
+            "_andFlg": 1,
+            "_gaiyoFlg": 1,
+            "_risyuFlg": 1,
+            "_excludeFukaikoFlg": 1,
+            "_eventId": None,
+            "outputFormat": None
+        }
 
-# download a csv
-csv_post = post.copy()
-csv_post["_eventId"] = "output"
-csv_post["outputFormat"] = 0
-response = session.post(do_url, data=csv_post)
+    def get_post(self) -> PostDict:
+        """Get a copy of PostDict
 
-# output
-date = datetime.datetime.now()
-csv_dir = "../csv"
-filename = "%s/kdb-%04d%02d%02d.csv" % (csv_dir, date.year, date.month, date.day)
+        Returns:
+            PostDict: Dict for requesting with POST to KdB
+        """
+        return self.post.copy()
 
-if not os.path.isdir(csv_dir):
-	os.mkdir(csv_dir)
+    def download(self, filename: str) -> None:
+        """Download KdB data
 
-with open(filename, "w", encoding="utf-8") as fp :
-	fp.write(response.text)
+        Args:
+            filename (str): [description]
+        """
+        self.__download()
+        open(filename, "w", encoding="utf-8").write(self.response_text)
+
+    def __download(self) -> None:
+        """Helper for downloading
+        """
+        self.__start_session()
+        self.__search_kdb()
+        self.__download_csv()
+
+    def __start_session(self) -> None:
+        """Helper for starting session to KdB
+        """
+        kdb_url = "https://kdb.tsukuba.ac.jp/"
+        self.session = requests.session()
+        self.response = self.session.get(kdb_url)
+
+    def __search_kdb(self) -> None:
+        """Helper for searching
+        """
+        search_post = self.get_post()
+        search_post["_eventId"] = "searchOpeningCourse"
+        self.response = self.session.post(self.response.url, data=search_post)
+        self.do_url = self.response.url
+
+    def __download_csv(self) -> None:
+        """Helper for downloading csv
+        """
+        csv_post = self.get_post()
+        csv_post["_eventId"] = "output"
+        csv_post["outputFormat"] = 0
+        self.response_text = self.session.post(self.do_url, data=csv_post).text
+
+
+def main() -> None:
+    """Main
+    """
+    import datetime
+    import os
+
+    date = datetime.datetime.now()
+    csv_dir = "../csv"
+    filename = "%s/kdb-%04d%02d%02d.csv" % (csv_dir,
+                                            date.year, date.month, date.day)
+
+    os.makedirs(csv_dir, exist_ok=True)
+    KdbDownloader().download(filename)
+
+
+if __name__ == '__main__':
+    main()
