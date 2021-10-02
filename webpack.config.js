1272bace787e2bde00bfb9f53e915813088ba5c5
chihiro sakai <sakaic2003@gmail.com>
2021-10-03 04:27:13 +0900

    productionにビルドできるようにwebpackのコンフィグを修正

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 const HTMLWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-  mode: 'development',
+  mode: process.env.NODE_ENV || 'production',
   entry: './src/index.ts',
   module: {
     rules: [
