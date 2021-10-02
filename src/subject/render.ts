341f09e1ff99ab8ba03416b61e1514a99730b9b6
chihiro sakai <sakaic2003@gmail.com>
2021-10-03 05:11:54 +0900

    Subjectを機能ごとに分割

---
+++ src/subject/render.ts
@@ -0,0 +1,72 @@
+import type { Subject } from '.';
+import { onBookmarkChanged } from '../bookmark';
+
+/**
+ * Subjectオブジェクトからtr要素を作るメソッド。旧Subject.createTrメソッド。
+ * @param {Subject} subject もとになるSubjectのインスタンス。
+ * @returns {HTMLTableRowElement} 表示するべきtr要素
+ */
+export function RenderSubjectAsTableRow(subject: Subject): HTMLTableRowElement {
+  const methods = ['対面', 'オンデマンド', '同時双方向'].filter(
+    (it) => subject.note.indexOf(it) > -1
+  );
+
+  const tr = document.createElement('tr');
+  const lineBreak = document.createElement('br');
+
+  const anchorOfficial = document.createElement('a');
+  anchorOfficial.href = `https://kdb.tsukuba.ac.jp/syllabi/2021/${subject.code}/jpn`;
+  anchorOfficial.className = 'syllabus';
+  anchorOfficial.target = '_blank';
+  anchorOfficial.append('シラバス');
+
+  const anchorMirror = document.createElement('a');
+  anchorMirror.href = `https://make-it-tsukuba.github.io/alternative-tsukuba-syllabus/syllabus/${subject.code}.html`;
+  anchorMirror.className = 'syllabus';
+  anchorMirror.target = '_blank';
+  anchorMirror.append('シラバス（ミラー)');
+
+  anchorMirror.addEventListener('click', (evt) => {
+    evt.preventDefault();
+    let win = document.createElement('draggable-window');
+    win.innerHTML = `<div slot='title'>${subject.name} - シラバス</div><iframe slot='body' src='${anchorMirror.href}' />`;
+    document.body.append(win);
+  });
+
+  const bookmarkCheckbox = document.createElement('input');
+  bookmarkCheckbox.type = 'checkbox';
+  bookmarkCheckbox.className = 'bookmark';
+  bookmarkCheckbox.addEventListener('click', onBookmarkChanged);
+  bookmarkCheckbox.id = `bookmark-${subject.code}`;
+  bookmarkCheckbox.value = subject.code;
+
+  tr.append(
+    createColumn(
+      subject.code,
+      lineBreak,
+      subject.name,
+      anchorOfficial,
+      anchorMirror,
+      bookmarkCheckbox
+    ),
+    createColumn(`${subject.credit}単位`, lineBreak, `${subject.year}年次`),
+    createColumn(subject.termStr, lineBreak, subject.periodStr),
+    createColumn(subject.room.replace(/,/g, '<br/>')),
+    createColumn(subject.person.replace(/,/g, '<br/>')),
+    methods.length < 1 ? createColumn('不詳') : createColumn(methods.join('<br/>')),
+    createColumn(subject.abstract),
+    createColumn(subject.note)
+  );
+
+  return tr;
+}
+
+/**
+ * ヘルパー関数
+ * @param {(string | Node)[]} content tdの中にchildrenとして入るDOM Nodeまたは文字列
+ */
+function createColumn(...content: (string | Node)[]) {
+  const td = document.createElement('td');
+  td.append(...content);
+  return td;
+}
---
+++ src/subject/search.ts
@@ -0,0 +1,83 @@
+import { getBookmarks } from '../bookmark';
+import type { Subject } from '.';
+import { Periods } from './period';
+
+export interface SearchOptions {
+  keyword: string;
+  reqA: string;
+  reqB: string;
+  reqC: string;
+  online: string;
+  year: string;
+  season: string | null;
+  module: string | null;
+  periods: Periods;
+  containsName: boolean;
+  containsCode: boolean;
+  containsRoom: boolean;
+  containsPerson: boolean;
+  containsAbstract: boolean;
+  containsBookmark: boolean;
+  concentration: boolean;
+  negotiable: boolean;
+  asneeded: boolean;
+}
+
+export function matchesSearchOptions(subject: Subject, options: SearchOptions): boolean {
+  // keyword
+  const regex = new RegExp(options.keyword);
+  const matchesCode = options.containsCode && subject.code.indexOf(options.keyword) > -1;
+  const matchesName = options.containsName && subject.name.match(regex) != null;
+  const matchesRoom = options.containsRoom && subject.room.match(regex) != null;
+  const matchesPerson = options.containsPerson && subject.person.match(regex) != null;
+  const matchesAbstract = options.containsAbstract && subject.abstract.match(regex) != null;
+  const matchesKeyword =
+    matchesCode || matchesName || matchesRoom || matchesPerson || matchesAbstract;
+
+  // period
+  const matchesPeriods =
+    options.periods.length == 0 ||
+    subject.periodsArray.reduce<boolean>(
+      (accumulator, periods) => accumulator || periods.matches(options.periods),
+      false
+    ) ||
+    (options.concentration && subject.concentration) ||
+    (options.negotiable && subject.negotiable) ||
+    (options.asneeded && subject.asneeded);
+
+  // standard year of course
+  let matchesYear;
+  if (options.year == 'null') {
+    matchesYear = true;
+  } else if (subject.year.indexOf('-') == -1) {
+    matchesYear = subject.year.indexOf(options.year) > -1;
+  } else {
+    let minYear = subject.year.replace(/\s-\s[1-6]/g, '');
+    let maxYear = subject.year.replace(/[1-6]\s-\s/g, '');
+    matchesYear = minYear <= options.year && options.year <= maxYear;
+  }
+
+  // requirements
+  let matchesReqA = options.reqA == 'null' || options.reqA == subject.reqA;
+  let matchesReqB = options.reqB == 'null' || options.reqB == subject.reqB;
+  let matchesReqC = options.reqC == 'null' || options.reqC == subject.reqC;
+
+  // other options
+  let matchesSeason = options.season == null || subject.termStr.indexOf(options.season) > -1;
+  let matchesModule = options.module == null || subject.termStr.indexOf(options.module) > -1;
+  let matchesOnline = options.online == 'null' || subject.note.indexOf(options.online) > -1;
+  let matchesBookmark = !options.containsBookmark || getBookmarks().includes(subject.code);
+
+  return (
+    matchesKeyword &&
+    matchesPeriods &&
+    matchesYear &&
+    matchesReqA &&
+    matchesReqB &&
+    matchesReqC &&
+    matchesSeason &&
+    matchesModule &&
+    matchesOnline &&
+    matchesBookmark
+  );
+}
